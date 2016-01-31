4e1a966b8feba681029d31e37e31258fd8fd3c83
Matthew Hicks <mdhicks@umich.edu>
2016-01-31 00:04:03 -0500

    Added ability to compare two SignalHistory objects for equality.

--- vcd/SignalHistory.java
+++ vcd/SignalHistory.java
@@ -84,4 +84,37 @@ public class SignalHistory extends Signal
     {
         return values;
     }
-}
+
+    /**
+     Returns true if the passed {@link vcd.SignalHistory} object has
+     the same values and times as this object.
+     @author Matthew Hicks
+     @param pSH A SignalHistory object to compare this one to.
+     @return true if the passed object has the same history, false otherwise.
+    */
+    public boolean equals(SignalHistory pSH)
+    {
+	// Perform the fast checks first
+	if(this.getValues().size() != pSH.getValues().size())
+	    return false;
+	if(this.getTimeLow() != pSH.getTimeLow())
+	    return false;
+	if(this.getTimeHigh() != pSH.getTimeHigh())
+	    return false;
+	if(this.getToggles() != pSH.getToggles())
+	    return false;
+	if(this.getWidth() != pSH.getWidth())
+	    return false;
+
+	// Now we have to check update-by-update
+	for(int vtt = 0; vtt < this.getValues().size(); ++ vtt)
+	{
+	    if(this.getValues().get(vtt).getTime() != pSH.getValues().get(vtt).getTime())
+		return false;
+	    if(!this.getValues().get(vtt).getValue().equals(pSH.getValues().get(vtt).getValue()))
+		return false;
+	}
+
+	return true;
+    }
+  }

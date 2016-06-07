bb86d9975d8a1be736139ba8aacbbd2897623dab
Matthew Hicks <mdhicks@umich.edu>
2016-06-06 21:41:53 -0400

    Update histogram plot and video gen script.

--- histogram.gp
+++ histogram.gp
@@ -3,9 +3,11 @@ set size 1.0,1.0;
 set output 'histogram.png';
 set grid xtics ytics;
 set xlabel 'Toggle Rate';
-set ylabel "Proportion of Signals";
+set ylabel "Proportion of Wires";
 unset key;
 set yrange [0.0:0.03]
-set xrange [0:0.5]
+set xrange [0:0.25]
 
-plot 'histogram.txt' using 3:4 with boxes lc rgb "blue" linetype 1 linewidth 4 title 'Base' ;
+set arrow from .17,0 to .17,.03 nohead lt 0 lw 5 lc rgb "red"
+
+plot 'histogram.txt' using 3:4 with boxes lc rgb "grey" linetype 1 linewidth 4 title 'Base' ;
--- makeHists.sh
+++ makeHists.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-for i in {1..100}
-do
+for i in {1..1000}; do
     cp hist_"$i".txt histogram.txt
     gnuplot histogram.gp
     mv histogram.png hist_"$i".png
-done
+done;
+
+# Combine the png images into a video
+
+# Version that is small and good quality
+ffmpeg -v quiet -i hist_%d.png -c:v libx264 -preset medium -qp 0 -r 30 -y histogram.mkv
+
+# Version that work well enough in PowerPoint
+ffmpeg -v quiet -i hist_%d.png -f lavfi -i aevalsrc=0 -shortest -c:v mpeg1video -qscale:v 2 -c:a libmp3lame -r 30 -y histogram.mpg
+
+for i in {1..1000}; do
+    rm hist_"$i".png
+done;
 
 rm histogram.txt
-
-ffmpeg -v quiet -i hist_%d.png -c:v libx264 -preset medium -qp 0 -r 24 -y histogram.mkv
